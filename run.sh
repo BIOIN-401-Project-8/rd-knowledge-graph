554f6767ac5bc9974c60e27f15f4497034179efb
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-25 19:48:20 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-docker compose run aioner bash -c "cd src && python AIONER_Run.py -i /data/pmc-open-access-subset/abstracts-bioc/ -m /data/Bioformer-softmax-AIONER/Bioformer-softmax-AIONER -v ../vocab/AIO_label.vocab -e ALL -o /data/pmc-open-access-subset/abstracts-ner"
-docker compose run taggerone ./run_Disease_BioCXML.sh /data/pmc-open-access-subset/abstracts-ner TaggerOne-0.3.0/data /data/pmc-open-access-subset/abstracts-taggerone-cellline
-docker compose run taggerone ./run_Disease_BioCXML.sh /data/pmc-open-access-subset/abstracts-taggerone-cellline TaggerOne-0.3.0/data /data/pmc-open-access-subset/abstracts-taggerone-disease
-docker compose run gnorm2 ./GNorm2.sh /data/pmc-open-access-subset/abstracts-taggerone-disease /data/pmc-open-access-subset/abstracts-gnorm2
-docker compose run nlmchem ./run_Chemical_BioCXML.sh /data/pmc-open-access-subset/abstracts-taggerone-disease CHEM_NORM/data/abbr_frequency_2020.json.gz /data/pmc-open-access-subset/abstracts-nlmchem
-docker compose run biorex python ./scripts/run_test_pred_bulk.py /data/pmc-open-access-subset/abstracts-pubtator /data/pmc-open-access-subset/abstracts-biorex
+python3 src/organize.py
+docker compose run aioner bash -c "cd src && python AIONER_Run.py -i /data/rgd-knowledge-graph/pubtator3/local/bioc/ -m ../pretrained_models/AIONER/Bioformer-softmax-AIONER/Bioformer-softmax-AIONER -v ../vocab/AIO_label.vocab -e ALL -o /data/rgd-knowledge-graph/pubtator3/local/aioner"
+python3 src/clean.py
+docker compose run taggerone ./run_CellLine_BioCXML.sh /data/rgd-knowledge-graph/pubtator3/local/aioner TaggerOne-0.3.0/data /data/rgd-knowledge-graph/pubtator3/local/taggerone-cellline
+docker compose run taggerone ./run_Disease_BioCXML.sh /data/rgd-knowledge-graph/pubtator3/local/aioner TaggerOne-0.3.0/data /data/rgd-knowledge-graph/pubtator3/local/taggerone-disease
+docker compose run gnorm2 ./GNorm2.sh /data/rgd-knowledge-graph/pubtator3/local/aioner /data/rgd-knowledge-graph/pubtator3/local/gnorm2
+docker compose run nlmchem bash -c "./run_Chemical_BioCXML.sh /data/rgd-knowledge-graph/pubtator3/local/aioner CHEM_NORM/data/abbr_frequency_2020.json.gz /data/rgd-knowledge-graph/pubtator3/local/nlmchem"
+docker compose run biorex python ./scripts/run_test_pred_bulk.py /data/rgd-knowledge-graph/pubtator3/local/nlmchem /data/rgd-knowledge-graph/pubtator3/local/biorex
+python3 src/ingest.py

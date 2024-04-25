97f45db2af032db03371776ab720283a47a687a0
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-25 00:00:51 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@ docker compose run aioner bash -c "cd src && python AIONER_Run.py -i /data/rgd-k
 docker compose run devcontainer python3 src/clean.py
 docker compose run taggerone ./run_CellLine_BioCXML.sh /data/rgd-knowledge-graph/pubtator3/local/aioner TaggerOne-0.3.0/data /data/rgd-knowledge-graph/pubtator3/local/taggerone-cellline
 docker compose run taggerone ./run_Disease_BioCXML.sh /data/rgd-knowledge-graph/pubtator3/local/aioner TaggerOne-0.3.0/data /data/rgd-knowledge-graph/pubtator3/local/taggerone-disease
-docker compose run gnorm2 python3 run_batches.py /data/rgd-knowledge-graph/pubtator3/local/aioner /data/rgd-knowledge-graph/pubtator3/local/gnorm2 --batch_size 8 --max_workers 8
+docker compose run gnorm2 python3 run_batches.py /data/rgd-knowledge-graph/pubtator3/local/aioner /data/rgd-knowledge-graph/pubtator3/local/gnorm2 --batch_size 8 --max_workers 2
 docker compose run nlmchem bash -c "./run_Chemical_BioCXML.sh /data/rgd-knowledge-graph/pubtator3/local/aioner CHEM_NORM/data/abbr_frequency_2020.json.gz /data/rgd-knowledge-graph/pubtator3/local/nlmchem"
-# mkdir -p /data/rgd-knowledge-graph/pubtator3/local/gnormplus
-# docker compose run gnorm2 java -Xmx60G -Xms30G -jar GNormPlus.jar /data/rgd-knowledge-graph/pubtator3/local/aioner /data/rgd-knowledge-graph/pubtator3/local/gnormplus setup.txt
-docker compose run gnorm2 python3 run_batches.py /data/rgd-knowledge-graph/pubtator3/local/aioner /data/rgd-knowledge-graph/pubtator3/local/gnorm2 --batch_size 128 --mode gnormplus
+docker compose run gnorm2 python3 run_batches.py /data/rgd-knowledge-graph/pubtator3/local/aioner /data/rgd-knowledge-graph/pubtator3/local/gnormplus --batch_size 64 --max_workers 3 --mode gnormplus
 mkdir -p /data/rgd-knowledge-graph/pubtator3/local/tmvar3
 docker compose run tmvar3 java -Xmx5G -Xms5G -jar tmVar.jar /data/rgd-knowledge-graph/pubtator3/local/gnormplus /data/rgd-knowledge-graph/pubtator3/local/tmvar3
 python3 src/merge.py
